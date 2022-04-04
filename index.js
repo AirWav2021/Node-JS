e330803ff7789ffb2640a79c125ba143b2fb949c
AirWav2021 <AirWav2015@gmail.com>
2022-04-05 01:29:12 +0300

    Выполнил ДЗ 4

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
-const fs = require('fs')
-const readline = require('readline')
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
 
-const readStream = fs.createReadStream('./access.log', 'utf8')
-const writeStream1 = fs.createWriteStream('./89.123.1.41_requests.log')
-const writeStream2 = fs.createWriteStream('./34.48.240.111_requests.log')
 
-const rl = readline.createInterface({
-	input: readStream,
-	terminal: true,
-})
 
-rl.on('line', line => {
-	if (line.includes('89.123.1.41')) {
-		writeStream1.write(line + '\n')
-	}
 
-	if (line.includes('34.48.240.111')) {
-		writeStream2.write(line + '\n')
-	}
-})
+const options = async() => {
+    const serchParams = { dirToSearch: '', pattern: '' };
+
+    const { thisDir } = await inquirer.prompt([{
+        name: 'thisDir',
+        type: 'confirm',
+        message: 'search here:',
+        describe: 'search here'
+    }]);
+
+    if (!thisDir) {
+        serchParams.dirToSearch = (await inquirer.prompt([{
+            name: 'dirToSearch',
+            type: 'input',
+            message: 'indicate the path: ',
+            describe: 'indicate the path'
+        }])).dirToSearch;
+    } else {
+        serchParams.dirToSearch = process.cwd();
+    }
+
+    serchParams.pattern = (await inquirer.prompt([{
+        name: 'pattern',
+        type: 'input',
+        message: 'Pattern: ',
+        describe: 'Pattern'
+    }])).pattern;
+
+    return serchParams;
+};
+
+
+const dir = (dirPath) => fs.lstatSync(dirPath).isDirectory();
+
+
+const run = async() => {
+    const { dirToSearch, pattern } = await options();
+    const files = [];
+    const dirsToResearch = [];
+    dirsToResearch.push(dirToSearch);
+
+    while (dirsToResearch.length > 0) {
+        const currentDir = dirsToResearch.shift();
+        const dirContains = fs.readdirSync(currentDir);
+        const inDirFiles = dirContains.filter((fileName) => fileName.indexOf(pattern) !== -1);
+        const inDirDirs = dirContains.map((dirName) => path.join(currentDir, dirName)).filter(dir);
+        files.push(...inDirFiles);
+        dirsToResearch.push(...inDirDirs);
+    }
+
+    console.log(files);
+};
+
+run();
